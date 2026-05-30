07c7fdfd28dc3002e30b62ff49c46f2db19dfa64
apocalypse7050@gmail.com <pota@codeindesign.kr>
2026-05-30 15:31:55 +0900

    test(category): add failing test for delete category with associated products

---
+++ src/test/java/gift/category/CategoryDeleteTest.java
@@ -0,0 +1,32 @@
+package gift.category;
+
+import gift.product.Product;
+import gift.product.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class CategoryDeleteTest {
+
+    @Autowired MockMvc mockMvc;
+    @Autowired CategoryRepository categoryRepository;
+    @Autowired ProductRepository productRepository;
+
+    @Test
+    void 상품이_있는_카테고리_삭제시_400() throws Exception {
+        Category category = categoryRepository.save(new Category("카테고리삭제_테스트", "#fff", "http://test.url", null));
+        productRepository.save(new Product("카테고리삭제_상품", 1000, "http://test.url", category));
+
+        mockMvc.perform(delete("/api/categories/" + category.getId()))
+            .andExpect(status().isBadRequest());
+    }
+}
