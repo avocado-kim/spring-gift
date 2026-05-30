7dce77d439daecda16fdeab5afb3675c3dd86953
apocalypse7050@gmail.com <pota@codeindesign.kr>
2026-05-30 15:23:35 +0900

    test(order): add failing test for wish cleanup after order completion

---
+++ src/test/java/gift/order/OrderWishCleanupTest.java
@@ -0,0 +1,56 @@
+package gift.order;
+
+import gift.category.Category;
+import gift.category.CategoryRepository;
+import gift.member.Member;
+import gift.member.MemberRepository;
+import gift.option.Option;
+import gift.option.OptionRepository;
+import gift.product.Product;
+import gift.product.ProductRepository;
+import gift.wish.Wish;
+import gift.wish.WishRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class OrderWishCleanupTest {
+
+    @Autowired OrderService orderService;
+    @Autowired WishRepository wishRepository;
+    @Autowired CategoryRepository categoryRepository;
+    @Autowired ProductRepository productRepository;
+    @Autowired OptionRepository optionRepository;
+    @Autowired MemberRepository memberRepository;
+
+    private Member member;
+    private Option option;
+    private Product product;
+
+    @BeforeEach
+    void setUp() {
+        Category category = categoryRepository.save(new Category("위시_테스트카테고리", "#ffffff", "http://test.url", null));
+        product = productRepository.save(new Product("위시테스트상품", 100, "http://test.url", category));
+        option = optionRepository.save(new Option(product, "위시테스트옵션", 10));
+        member = memberRepository.save(new Member("wish_cleanup@example.com", "pw"));
+        member.chargePoint(10000);
+        memberRepository.save(member);
+        wishRepository.save(new Wish(member.getId(), product));
+    }
+
+    @Test
+    void 주문_완료_후_위시리스트_삭제() {
+        assertThat(wishRepository.findByMemberIdAndProductId(member.getId(), product.getId())).isPresent();
+
+        orderService.createOrder(member, option.getId(), 1, null);
+
+        assertThat(wishRepository.findByMemberIdAndProductId(member.getId(), product.getId())).isEmpty();
+    }
+}
