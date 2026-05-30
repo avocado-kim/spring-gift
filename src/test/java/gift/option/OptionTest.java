2234981981ac785b81d1cef728eceb577cf3bba5
apocalypse7050@gmail.com <pota@codeindesign.kr>
2026-05-30 14:30:26 +0900

    test(option): add unit tests for Option subtractQuantity

---
+++ src/test/java/gift/option/OptionTest.java
@@ -0,0 +1,39 @@
+package gift.option;
+
+import gift.category.Category;
+import gift.product.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class OptionTest {
+
+    private Option option;
+
+    @BeforeEach
+    void setUp() {
+        Category category = new Category("테스트", "#000000", "http://img.url", null);
+        Product product = new Product("상품", 1000, "http://img.url", category);
+        option = new Option(product, "기본옵션", 10);
+    }
+
+    @Test
+    void subtractQuantity_정상_차감() {
+        option.subtractQuantity(3);
+        assertThat(option.getQuantity()).isEqualTo(7);
+    }
+
+    @Test
+    void subtractQuantity_전량_차감() {
+        option.subtractQuantity(10);
+        assertThat(option.getQuantity()).isEqualTo(0);
+    }
+
+    @Test
+    void subtractQuantity_재고_초과_예외() {
+        assertThatThrownBy(() -> option.subtractQuantity(11))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+}
